cff1e6b1ce04c21562f5aa51fd62de1acede8b7a
rohan-repos <rohannagendra36@gmail.com>
2020-12-01 20:20:57 -0500

    Added tabs

--- src/Components/passwordgenerator/Main.js
+++ src/Components/passwordgenerator/Main.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import Header from './header/Header';
-import Display from './display/Display';
+import React from "react";
+import Header from "./header/Header";
+import Display from "./display/Display";
+import DisplayStrength from "../passwordstrengthchecker/DisplayStrength";
+import { Tabs, Tab } from "react-bootstrap";
 
 const Main = () => {
-  const contStyle={
-    display:"flex",
+  const contStyle = {
+    display: "flex",
     // alignItems:"center",
-    justifyContent:"center",
-    minHeight:"100vh"
-  }
+    justifyContent: "center",
+    minHeight: "100vh",
+  };
 
-    return (
-        <div style={contStyle}>
-          <div>
+  return (
+    <div style={contStyle}>
+      <div style={{maxWidth:"1200px"}}>
+        <Tabs defaultActiveKey="generatepassword" id="uncontrolled-tab-example">
+          <Tab eventKey="generatepassword" title="Generate Password">
             <Header />
             <Display />
-            </div>
-        </div>
-    )
-}
+          </Tab>
+          <Tab eventKey="passwordchecker" title="Password Checker">
+            <DisplayStrength />
+          </Tab>
+        </Tabs>
+        
+      </div>
+    </div>
+  );
+};
 
 export default Main;
